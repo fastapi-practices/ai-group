f4129163b6a5b42f4c9c51d885035eafc649442c
Wu Clan <jianhengwu0407@gmail.com>
2026-09-06 23:00:04 +0800

    Update plugin menu SQL paths

--- sql/mysql/init.sql
+++ sql/mysql/init.sql
@@ -1,7 +1,7 @@
 set @ai_menu_id = (select id from sys_menu where name = 'PluginAIBuddy');
 
 insert into sys_menu (title, name, path, sort, icon, type, component, perms, status, display, cache, link, remark, parent_id, created_time, updated_time)
-values ('分组管理', 'AIGroupManage', '/plugins/ai_buddy/group', 7, 'material-symbols:group-outline', 1, '/plugins/ai_buddy_group/views/index', null, 1, 1, 1, '', null, @ai_menu_id, now(), null);
+values ('ai-buddy-group.menu', 'AIGroupManage', '/plugins/ai-buddy/group', 7, 'material-symbols:group-outline', 1, '/plugins/ai-buddy-group/views/index', null, 1, 1, 1, '', null, @ai_menu_id, now(), null);
 
 set @ai_group_menu_id = LAST_INSERT_ID();
 
--- sql/postgresql/init_snowflake.sql
+++ sql/postgresql/init_snowflake.sql
@@ -1,5 +1,5 @@
 insert into sys_menu (id, title, name, path, sort, icon, type, component, perms, status, display, cache, link, remark, parent_id, created_time, updated_time)
-values (2147098509659213900, '分组管理', 'AIGroupManage', '/plugins/ai_buddy/group', 7, 'material-symbols:group-outline', 1, '/plugins/ai_buddy_group/views/index', null, 1, 1, 1, '', null, (select id from sys_menu where name = 'PluginAIBuddy'), now(), null);
+values (2147098509659213900, 'ai-buddy-group.menu', 'AIGroupManage', '/plugins/ai-buddy/group', 7, 'material-symbols:group-outline', 1, '/plugins/ai-buddy-group/views/index', null, 1, 1, 1, '', null, (select id from sys_menu where name = 'PluginAIBuddy'), now(), null);
 
 insert into sys_menu (id, title, name, path, sort, icon, type, component, perms, status, display, cache, link, remark, parent_id, created_time, updated_time)
 values
